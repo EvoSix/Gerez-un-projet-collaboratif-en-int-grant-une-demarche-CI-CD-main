61a8634574571009ba2642b5c152739666cd12f6
EvoSix <49368909+EvoSix@users.noreply.github.com>
2025-06-26 23:17:28 +0200

    Update karma.conf.js

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
     client: {
       jasmine: {
         // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+      
        
        
       },
